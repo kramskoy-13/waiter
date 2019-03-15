566a8294f231d408f88b6a8fb33728780515211b
Dmitriy Kramskoy <dkramskoy.13@gmail.com>
2019-03-15 19:48:20 +0300

    ...

--- js/index.js
+++ js/index.js
@@ -3,29 +3,34 @@ document.addEventListener("DOMContentLoaded", function() {
     let text = 'Hello! Today I Will Be Your Waiter!';
     let length = 0;
 
-    let textContainer = document.getElementById('textContainer');
-
-    textContainer.parentNode.classList.remove('initial');
-
+    let textContainer 	 = document.getElementById('textContainer');
+	let initialContainer = document.getElementById('initialContainer');
+	
+	setTimeout(function() {
+        textContainer.parentNode.classList.remove('initial');
+    }, 250);
     setTimeout(function() {
         textContainer.parentNode.classList.add('loaded');
     }, 500);
 
-    // function showText(piece) {
-    //     if(text.length <= length) {
-            setTimeout(function() {
-                textContainer.parentNode.classList.add('completed');
-            }, 3500);
-    //     }
-    //     else textContainer.innerHTML += piece;
-    //     length++;
-    //     setTimeout(function() {
-    //         showText(text[length]);
-    //     }, 100)
-    // }
-    // setTimeout(function() {
-    //     return showText(text[length]);
-    // }, 500);
-
+     function showText(piece) {
+         if(text.length <= length) {
+             setTimeout(function() {
+                 textContainer.classList.add('completed');
+				 setTimeout(function(){
+					 initialContainer.classList.add('to-login');
+				 }, 1000)
+             }, 1500);
+         }
+         else textContainer.innerHTML += piece;
+         length++;
+         setTimeout(function() {
+             showText(text[length]);
+         }, 100)
+     }
+     setTimeout(function() {
+         return showText(text[length]);
+     }, 1500);
+	
 });
 
