6835f4a610b98c4b84009ae47b9adcf7c0aa9bba
kramskoy <dkramskoy.13@gmail.com>
2019-07-14 22:47:48 +0300

    svg icons added

--- js/view/Template/templates/_categories.js
+++ js/view/Template/templates/_categories.js
@@ -3,10 +3,17 @@ export const CATEGORIES_TEMPLATE = ({ categories }) => {
     categories.forEach(category => { 
         const icon = category.icon();
         const id = category.name.replace(/ /g, '_');
-        insert += `<div class="categories__container_item" id="${id}">${icon}</div>`
+        insert += `
+        <div class="categories__container_item" id="${id}">
+            ${icon}
+            <div class="name">${category.name}</div>
+            <div class="select hidden">Select</div>
+        </div>
+        `
     })
     return `
     <div class="categories__container two">
+        <h4 class="h4">Categories</h4>
         ${insert}
     </div>
  `
